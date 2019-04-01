f7e56d95223c74a581d5dabfc08b66d0dfca37ec
Jaime Tyler <jaimetyler88@gmail.com>
2019-04-01 00:36:45 -0400

    got all the user unput being saved to the database to show to the page on the schedule. Still a little bit confused on the timestamp. I think all i have left to do is figure out how i am going to calculate the next arrival and minutes away columns and that should be it besides styling/

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,20 +19,58 @@ var config = {
   var frequency = 0;
   var firstTrainTime = 0;
 
+  
+  
   //click event that will add the user's input to the database
   $("#add-train").on("click", function(){
-    trainName = $("#name-input").val().trim();
-    destination = $("#destination-input").val().trim();
-    frequency = $("#frequency-input").val().trim();
-    firstTrainTime = $("#frequency-input").val().trim();
-    
-    //push data to firebase
-    database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        frequency: frequency,
-        firstTrainTime: firebase.database.ServerValue.TIMESTAMP
+      event.preventDefault();
+      trainName = $("#name-input").val().trim();
+      destination = $("#destination-input").val().trim();
+      frequency = $("#frequency-input").val().trim();
+      firstTrainTime = $("#frequency-input").val().trim();
+      
+      //?????????????????????why cant i conole.log() here???????? All user unput is getting pushed to firebase just fine, so not sure why its not working//
+      console.log(trainName);
+      console.log(destination);
+      console.log(frequency);
+      console.log(firstTrainTime);
+      ////////////////???????????????????????????????????????/////////////////////
+      
+      
+      //push data to firebase
+      database.ref().push({
+          trainName: trainName,
+          destination: destination,
+          frequency: frequency,
+          firstTrainTime: firebase.database.ServerValue.TIMESTAMP
+          //dateAdded: firebase.database.ServerValue.TIMESTAMP
+        })
     })
- 
-  })
+    
+    database.ref().on("child_added", function(snapshot){
+        var sv = snapshot.val();
 
+        //var convertedTrainTime = moment(childSnapshot.val(), firstTrainTime, "HH:mm").subtract(1, "years");
+        //console.log("convertedTime" + convertedTrainTime);
+        console.log(sv.trainName);
+        console.log(sv.destination);
+        console.log(sv.frequency);
+        console.log(sv.firstTrainTime);
+        
+        //var currentTime = moment();
+
+      var appendTable = `
+      <tr> 
+    <th scope="row">` + sv.trainName + `</th>
+    <td>${sv.destination}</td>
+    <td>${sv.frequency}</td>
+    <td>TBA</td>
+    <td>TBA</td>
+  </tr>` 
+
+  $(".new-train").append(appendTable);
+  },
+  function(errorObject){
+    console.log("errors handled: " + errorObject.code);
+  }
+  )
